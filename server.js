c394c6f8e0d0f238e29eab17c257fe85e375648b
RaresTudorache <tudrares@gmail.com>
2024-03-12 15:50:13 +0200

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./db');
-const menuRoutes = require('./routes/menuRoutes'); 
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db");
+const path = require("path");
+const menuRoutes = require("./routes/menuRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -10,8 +11,14 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use('/', menuRoutes);
+app.use("/", menuRoutes);
+
+app.use(express.static(path.join(__dirname, "./zano")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./zano", "index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
