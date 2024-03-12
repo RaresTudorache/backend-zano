6fce311c4c7dc4b5b27fca01f9bba5572abdb4f8
RaresTudorache <tudrares@gmail.com>
2024-03-12 18:12:45 +0200

    deploy

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/", menuRoutes);
 
-app.use(express.static(path.join(__dirname, "./zano")));
+app.use(express.static(path.join(__dirname, "./zano/zano")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./zano", "index.html"));
+    res.sendFile(path.join(__dirname, "./zano/zano", "index.html"));
 });
 
 app.listen(PORT, () => {
